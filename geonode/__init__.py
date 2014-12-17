6a51f97cc6e36132b7c30e4d80dfa280542753f9
Tyler Garner <garnertb@gmail.com>
2014-12-17 12:00:04 -0500

    Initial celery integration.

--- geonode/__init__.py
+++ geonode/__init__.py
@@ -18,6 +18,8 @@
 #########################################################################
 
 import os
+from .celery import app as celery_app  # flake8: noqa
+
 __version__ = (2, 4, 0, 'alpha', 0)
 
 
---
+++ geonode/tasks/update.py
@@ -0,0 +1,26 @@
+from celery.task import task
+from geonode.geoserver.helpers import gs_slurp
+from geonode.documents.models import Document
+
+
+@task(name='geonode.tasks.update.geoserver_update_layers', queue='update')
+def geoserver_update_layers(*args, **kwargs):
+    """
+    Runs update layers.
+    """
+    return gs_slurp(*args, **kwargs)
+
+
+@task(name='geonode.tasks.update.create_document_thumbnail', queue='update')
+def create_document_thumbnail(object_id):
+    """
+    Runs the create_thumbnail logic on a document.
+    """
+
+    try:
+        document = Document.objects.get(id=object_id)
+
+    except Document.DoesNotExist:
+        return
+
+    document.create_thumbnail()
